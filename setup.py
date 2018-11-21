a45914343340ecc6c40a786540d84b1b55ba7148
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-11-21 11:28:17 -0800

    version up to 0.1.1 for minor readme change

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     url='https://github.com/AgeOfLearning/coeus-python-framework',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
