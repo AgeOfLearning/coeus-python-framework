f3fb15505938d6ab5c1319b1170baa97acfe811b
Devon Klompmaker <devon.klompmaker@aofl.com>
2018-11-26 12:05:15 -0800

    renaming coeus -> coeus_test

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     url='https://github.com/AgeOfLearning/coeus-python-framework',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
